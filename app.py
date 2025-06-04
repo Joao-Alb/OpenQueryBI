c4ce3bf892b899e7c7b78dabbe155ab6314563e1
Joao-Alb <joao.lima.alb@gmail.com>
2025-06-03 23:55:47 -0300

    refractor: WIP generic database class

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import sqlite3
+import utils
 import sys
 import time
 
@@ -9,40 +9,35 @@ functions = {
     "plot_bar_from_sql": st.line_chart
 }
 
-def get_dataframe_from_sql(database_path:str,query:str,limit:int=100):
+def get_dataframe_from_sql(database_name:str,query:str,limit:int=100):
     """Get a dataframe from a SQL query. This will return a dataframe with the result of the query.
     """
     if "limit" not in query.lower():
         query = f"{query} LIMIT {limit}"
-    conn = sqlite3.connect(database_path)
-    cursor = conn.cursor()
-    cursor.execute(query)
-    result = cursor.fetchall()
-    columns = [column[0] for column in cursor.description]
-    conn.close()
-    return pd.DataFrame(result, columns=columns)
+    db_info = utils.get_database_info(database_name,"databases.json")
+    return utils.get_db(db_info).query(query)
 
-def plot_line_from_sql(database_path:str,query:str,x:str,y:str,limit:int=100):
+def plot_line_from_sql(database_name:str,query:str,x:str,y:str,limit:int=100):
     """Plot a line chart from a SQL query. This will create a line chart with the x and y values.
     """
-    df = get_dataframe_from_sql(database_path,query,limit)
+    df = get_dataframe_from_sql(database_name,query,limit)
     if x not in df.columns or y not in df.columns:
         raise ValueError(f"Columns {x} and {y} must be present in the dataframe.")
     return st.line_chart(df.set_index(x)[y])
 
-def plot_bar_from_sql(database_path:str,query:str,x:str,y:str,limit:int=100):
+def plot_bar_from_sql(database_name:str,query:str,x:str,y:str,limit:int=100):
     """Plot a bar chart from a SQL query using Streamlit. This will create a bar chart with the x and y values.
     """
-    df = get_dataframe_from_sql(database_path,query,limit)
+    df = get_dataframe_from_sql(database_name,query,limit)
     st.write(df)
     if x not in df.columns or y not in df.columns:
         raise ValueError(f"Columns {x} and {y} must be present in the dataframe.")
     return st.bar_chart(df.set_index(x)[y])
 
-def plot_from_sql(plot_type:str,database_path:str,query:str,x:str,y:str,update_interval:int = 180, limit:int=100):
+def plot_from_sql(plot_type:str,database_name:str,query:str,x:str,y:str,update_interval:int = 180, limit:int=100):
     """Plot a bar chart from a SQL query using Streamlit. This will create a bar chart with the x and y values.
     """
-    df = get_dataframe_from_sql(database_path,query,limit)
+    df = get_dataframe_from_sql(database_name,query,limit)
     if x not in df.columns or y not in df.columns:
         raise ValueError(f"Columns {x} and {y} must be present in the dataframe.")
     
@@ -55,14 +50,14 @@ def plot_from_sql(plot_type:str,database_path:str,query:str,x:str,y:str,update_i
 if __name__ == "__main__":
     if len(sys.argv) <= 1:
         st.write("No arguments provided. Running default plot.")
-        plot_from_sql("plot_bar_from_sql","test\inventory_management.db", "SELECT product_name, COUNT(DISTINCT supplier_id) as supplier_count FROM inventory JOIN supplier_products ON inventory.product_ID = supplier_products.product_id GROUP BY product_name ORDER BY supplier_count DESC", "product_name", "supplier_count",update_interval=10)
+        plot_from_sql("plot_bar_from_sql","inventory_management", "SELECT product_name, COUNT(DISTINCT supplier_id) as supplier_count FROM inventory JOIN supplier_products ON inventory.product_ID = supplier_products.product_id GROUP BY product_name ORDER BY supplier_count DESC", "product_name", "supplier_count",update_interval=10)
     if len(sys.argv) > 1:
         plot_type = sys.argv[1]
-        database_path = sys.argv[2]
+        database_name = sys.argv[2]
         query = sys.argv[3]
         x = sys.argv[4]
         y = sys.argv[5]
         limit = int(sys.argv[6]) if len(sys.argv) > 6 else 100
         update_interval = int(sys.argv[7]) if len(sys.argv) > 7 else 180
         st.write(sys.argv[8] if len(sys.argv) > 8 else "Graph requested to AI")
-        plot_from_sql(plot_type,database_path,query,x,y)
+        plot_from_sql(plot_type,database_name,query,x,y)
--- utils.py
+++ utils.py
@@ -6,7 +6,7 @@ def export_config(config:dict,path):
     with open(path+"\\config.json", 'w') as f:
         json.dump(config, f, indent=4)
 
-def get_database_info(database_name:str,databases_config_path):
+def get_database_info(database_name:str,databases_config_path)->dict:
     """Get the information of the database. This will return a dictionary with the name and description of the database.
     """
     with open(databases_config_path, 'r') as f:
@@ -16,7 +16,7 @@ def get_database_info(database_name:str,databases_config_path):
                 return database
     raise ValueError(f"Database {database_name} not found in {databases_config_path}")
 
-def get_databases(databases_config_path:str):
+def get_databases_list(databases_config_path:str):
     """Get the list of all the databases. This will return a list of dictionaries with the name and description of each database.
     """
     with open(databases_config_path, 'r') as f:
@@ -40,3 +40,95 @@ def clean_query(query:str):
     """Clean the query. This will remove the limit and order by clauses from the query.
     """
     return query.replace('"', '\\"').replace('\n', ' ').replace("'","")
+
+import sqlite3
+import pandas as pd
+class Database():
+    def __init__(self):
+        raise NotImplementedError("Database class is not implemented, use one of its childs.")
+    
+    def get_tables(self):
+        """Get the list of all the tables in the database. This will return a list of dictionaries with the name and description of each table.
+        """
+        raise NotImplementedError("get_tables method is not implemented, use one of its childs.")
+    
+    def get_table_columns(self):
+        """Get the columns of a table in the database. This will return the name of each column.
+        """
+        raise NotImplementedError("get_table_columns method is not implemented, use one of its childs.")
+    
+    def get_database_info(self):
+        """Get the information of the database. This will return the tables and columns of the database.
+        """
+        raise NotImplementedError("get_database_info method is not implemented, use one of its childs.")
+    
+    def query(self):
+        """Query the database. This will return a dataframe with the result of the query.
+        """
+        raise NotImplementedError("validate_query method is not implemented, use one of its childs.")
+
+class SQLiteDatabase(Database):
+    
+    def with_sqlite_connection(func):
+        """Decorator that handles SQLite connection management.
+        Opens connection, creates cursor, executes function, and ensures proper cleanup.
+        """
+        def wrapper(self, *args, **kwargs):
+            self.conn = sqlite3.connect(self.path)
+            self.cursor = self.conn.cursor()
+            try:
+                return func(self, *args, **kwargs)
+            finally:
+                self.cursor.close()
+                self.conn.close()
+        return wrapper
+
+
+    def __init__(self, name:str, path:str):
+        self.name = name
+        self.path = path
+        self.type = "sqlite"
+
+    @with_sqlite_connection
+    def get_tables(self):
+        self.cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+        tables = [row[0] for row in self.cursor.fetchall()]
+        return tables
+    
+    @with_sqlite_connection
+    def get_table_columns(self, table:str):
+        self.cursor.execute(f"PRAGMA table_info({table})")
+        columns = [row[1] for row in self.cursor.fetchall()]
+        return columns
+    
+    @with_sqlite_connection
+    def get_database_info(self):
+        """Get the information of the database. This will return the tables and columns of the database.
+        """
+        self.cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+        tables = [row[0] for row in self.cursor.fetchall()]
+        db_tables = ""
+        for table in tables:
+            db_tables += table + '(Columns: '+self.get_table_columns(table)+')\n'
+        return f"""
+        Database Name: {self.name}
+        Type: {self.type}
+        Tables: {db_tables}"""
+    
+    def query_string(self, query:str):
+        return self.query(query).to_string(index=False)
+    
+    @with_sqlite_connection
+    def query(self, query:str):
+        self.cursor.execute(query)
+        result = self.cursor.fetchall()
+        columns = [column[0] for column in self.cursor.description]
+        return pd.DataFrame(result, columns=columns)
+    
+def get_db(database_info:str)-> Database:
+    """Get the database object. This will return a database object based on the type of the database.
+    """
+    if database_info["type"] == "sqlite":
+        return SQLiteDatabase(database_info["name"], database_info["path"])
+    else:
+        raise ValueError(f"Database type {database_info['type']} is not supported.")
